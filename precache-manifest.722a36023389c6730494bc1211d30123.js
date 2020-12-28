9870befc3950d94dbf6190f15ee818b5f447f605
JESÚS FONTECHA DIEZMA <jesus.fontecha@uclm.es>
2020-12-28 16:40:34 +0100

    Updates

---
+++ precache-manifest.722a36023389c6730494bc1211d30123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ea3de9795ce74bc40f87583f0d2794",
+    "url": "/react-google-authentication/index.html"
+  },
+  {
+    "revision": "26e390155acdc8a2425f",
+    "url": "/react-google-authentication/static/css/main.5f0b32d2.chunk.css"
+  },
+  {
+    "revision": "2e4166f03d98686049f8",
+    "url": "/react-google-authentication/static/js/2.5b02833e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-google-authentication/static/js/2.5b02833e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e390155acdc8a2425f",
+    "url": "/react-google-authentication/static/js/main.aad74c9c.chunk.js"
+  },
+  {
+    "revision": "ac12cda5d4a95756c952",
+    "url": "/react-google-authentication/static/js/runtime-main.000499cd.js"
+  }
+]);
