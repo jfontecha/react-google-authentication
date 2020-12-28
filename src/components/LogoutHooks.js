b437d341d8cb429736200dadcf68bea35d2e4789
JESÚS FONTECHA DIEZMA <jesus.fontecha@uclm.es>
2020-12-28 16:48:49 +0100

    env variables

--- src/components/LogoutHooks.js
+++ src/components/LogoutHooks.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useGoogleLogout } from 'react-google-login';
 
-const clientId =
-  '654966311312-f5rr55j1tccrs4mbo19mdrpk73qd4ud4.apps.googleusercontent.com';
+const clientId = process.env.CLIENTID;
 
 function LogoutHooks() {
   const onLogoutSuccess = (res) => {
